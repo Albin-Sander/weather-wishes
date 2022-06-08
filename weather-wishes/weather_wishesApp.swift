14a54e7dd784294ed07d752e70bad8a9c4045bf7
Albin <albin.sander@collectorbank.se>
2022-06-08 17:40:54 +0200

    Initial Commit

---
+++ weather-wishes/weather_wishesApp.swift
@@ -0,0 +1,17 @@
+//
+//  weather_wishesApp.swift
+//  weather-wishes
+//
+//  Created by Albin Sander on 2022-06-08.
+//
+
+import SwiftUI
+
+@main
+struct weather_wishesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
