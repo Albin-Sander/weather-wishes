787ae09b46798def2cf31a4aac99a6e2d13501e3
Albin Sander <51294735+Albin-Sander@users.noreply.github.com>
2023-04-28 15:40:05 +0200

    Added searchtextfield

--- weather-wishes/WeatherView.swift
+++ weather-wishes/WeatherView.swift
@@ -10,86 +10,118 @@ import SwiftUI
 import WeatherKit
 
 struct WeatherView: View {
+    enum FocusedField {
+            case searchText
+        }
     @Environment(\.scenePhase) var scenePhase
     @StateObject var viewModel = WeatherViewModel()
     let iconColor = WeatherIconColor()
+    @State private var showTextfield = false
+   @State var searchText = ""
+    @FocusState private var focusedField: FocusedField?
     
     var body: some View {
-        List(0 ..< 1) { _ in
-       
-            if let weather = viewModel.weather {
-                VStack {
-                    Spacer()
-                    Text(LocationManager.shared.city!)
-                        .font(.system(size: 35.0))
-                        .fontWeight(.bold)
-          
-                    Spacer()
-                    Image(systemName: "\(weather.currentWeather.symbolName).fill")
-                        .font(.custom("Helvetica Neue", size: 130))
-             
-                        .foregroundStyle(iconColor.setIconColor(icon: weather.currentWeather.symbolName)[0], iconColor.setIconColor(icon: weather.currentWeather.symbolName).count > 1 ? iconColor.setIconColor(icon: weather.currentWeather.symbolName)[1] : Color.black)
-                        .onAppear {
-                            print(weather.currentWeather.symbolName)
-                        }
-                    Spacer()
-                    HStack {
-                        Text(weather
-                            .currentWeather.temperature.formatted()
-                        )
-                            
-                        .font(.system(size: 35.0))
-                        .fontWeight(.bold)
-                    }
-                    HStack {
-                        Text("Feels like temperature:")
-                        Text(weather.currentWeather.apparentTemperature
-                            .formatted())
-                    }
-                    .padding(.top)
-                    .font(.system(size: 20))
-                
-                    HStack {
-                        Text("Wind")
-                        Text(weather.currentWeather.wind.speed.formatted())
-                    }
-
-                    HourlyForecastView(hourWeatherlist: viewModel.hourlyWeatherData)
-               
-                    Spacer()
-                    
-                }
-                .onChange(of: scenePhase) { newPhase in
+        NavigationView {
+            List(0 ..< 1) { _ in
+           
+                if let weather = viewModel.weather {
+                    Section {
+                        VStack {
+                            Spacer()
+                            Text(LocationManager.shared.city!)
+                                .font(.system(size: 35.0))
+                                .fontWeight(.bold)
+                  
+                            Spacer()
+                            Image(systemName: "\(weather.currentWeather.symbolName).fill")
+                                .font(.custom("Helvetica Neue", size: 130))
+                     
+                                .foregroundStyle(iconColor.setIconColor(icon: weather.currentWeather.symbolName)[0], iconColor.setIconColor(icon: weather.currentWeather.symbolName).count > 1 ? iconColor.setIconColor(icon: weather.currentWeather.symbolName)[1] : Color.black)
+                                .onAppear {
+                                    print(weather.currentWeather.symbolName)
+                                }
+                            Spacer()
+                            HStack {
+                                Text(weather
+                                    .currentWeather.temperature.formatted()
+                                )
+                                    
+                                .font(.system(size: 35.0))
+                                .fontWeight(.bold)
+                            }
+                            HStack {
+                                Text("Feels like temperature:")
+                                Text(weather.currentWeather.apparentTemperature
+                                    .formatted())
+                            }
+                            .padding(.top)
+                            .font(.system(size: 20))
                         
-                    if newPhase == .active {
-                        Task {
+                            HStack {
+                                Text("Wind")
+                                Text(weather.currentWeather.wind.speed.formatted())
+                            }
+
+                            HourlyForecastView(hourWeatherlist: viewModel.hourlyWeatherData)
+                       
+                            Spacer()
+                            
+                        }
+                        .onChange(of: scenePhase) { newPhase in
+                                
+                            if newPhase == .active {
+                                Task {
+                                    await viewModel.getWeather()
+                                }
+                            }
+                    }
+                    }
+                    VStack(alignment: .leading) {
+                        Text("Upcoming days")
+                            .font(.headline)
+                                .padding(.leading, 5)
+                        Divider()
+                    }
+                    .listRowSeparator(.hidden)
+              
+                    SevenDayForeCastView(dayWeatherList: weather.dailyForecast.forecast)
+                    
+         
+                    
+                } else {
+                    ProgressView()
+                        .task {
                             await viewModel.getWeather()
                         }
-                    }
                 }
-                VStack(alignment: .leading) {
-                    Text("Next week")
-                        .font(.headline)
-                            .padding(.leading, 5)
-                    Divider()
-                }
-                .listRowSeparator(.hidden)
-          
-                SevenDayForeCastView(dayWeatherList: weather.dailyForecast.forecast)
                 
-            } else {
-                ProgressView()
-                    .task {
-                        await viewModel.getWeather()
-                    }
             }
-            
+            .listStyle(.insetGrouped)
+            .listRowSeparator(.hidden)
+            .refreshable {
+                Task {
+                    await viewModel.getWeather()
+                }
         }
-        .listRowSeparator(.hidden)
-        .refreshable {
-            Task {
-                await viewModel.getWeather()
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    HStack {
+                        if showTextfield {
+                            TextField("Search city",text: $searchText)
+                                .focused($focusedField, equals: .searchText)
+                        }
+                        Button(action: {
+                            showTextfield.toggle()
+                            focusedField = .searchText
+                        }) {
+                            Image(systemName: "magnifyingglass")
+                        }
+                        
+                     
+                    }
+                }
             }
+     
         }
     }
 }
